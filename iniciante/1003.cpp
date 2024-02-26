fa76cbbd185f48a178988e0e61c9221be69f8f59
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-02-26 15:44:07 -0300

    Soma Simples

---
+++ iniciante/1003.cpp
@@ -0,0 +1,15 @@
+//Soma Simples
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1, n2, soma = 0;
+
+    cin >> n1 >> n2;
+    soma += n1 + n2;
+    
+    cout << "SOMA = " << soma << endl;
+
+    return 0;
+}
